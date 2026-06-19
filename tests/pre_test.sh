255932eef0291c72de4806c529fcdff07066d9a9
dsamaey <diether.samaey@vito.be>
2026-06-19 13:33:16 +0200

    chore: enable Jenkins tests

---
+++ tests/pre_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# pre-test procedure specific for VITO Jenkins CI toolchain
+# executed just before running the tests
+
+set -eux
+pwd
+
+# do nothing (for now) but this file must be present for the Jenkins CI toolchain to work
